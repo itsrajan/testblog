88b60b32756319eae91f8765df3dbd035dd2fc96
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-04-18 10:33:02 -0400

    Add files via upload

---
+++ src/components/SocialList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Twitter from "../assets/twitter-alt.svg";
+import GitHub from "../assets/github-alt.svg";
+import config from "../lib/config";
+
+export function SocialList({}) {
+  return (
+    <div>
+      
+     
+      <style jsx>{`
+        a {
+          display: inline-block;
+        }
+        a:not(:last-child) {
+          margin-right: 2em;
+        }
+      `}</style>
+    </div>
+  );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "normalize.css";
+import { AppProps } from "next/app";
+
+import "../../public/styles/global.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
